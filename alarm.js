8423e48a18297e38ec20080c33575650a2584fd0
rax1mjon <tulaganovraximjon65@gmail.com>
2025-05-14 14:03:22 +0500

    firth commit

---
+++ alarm.js
@@ -0,0 +1,11 @@
+let SetAlarm = document.querySelector(".SetAlarm");
+let setAlarmForm = document.querySelector(".setAlarmForm");
+
+setAlarmForm.addEventListener("submit", function (e) {
+
+  console.log(e.target);
+
+  // if (NowDate == alarmValue) {
+  //   console.log(alarmValue, NowDate);
+  // }
+});
