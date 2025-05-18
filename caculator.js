e755fc0f9d74cabec18733d3c9c0bea59b25f41b
rax1mjon <tulaganovraximjon65@gmail.com>
2025-05-18 16:49:34 +0500

    change calculatro some sytle

--- caculator.js
+++ caculator.js
@@ -8,11 +8,53 @@ button.forEach((btn) => {
 });
 
 function calculator(display, id) {
-  let ArithmeticOperators = ["ac", "plus-minus", "percent", "divide"];
+  let ArithmeticOperators = ["/", "*", "-", "+"];
+  let otherOperators = ["ac", "plus-minus", "percent"];
+  let operator;
+  let result = 0;
 
-  if (!ArithmeticOperators.includes(id)) {
-    console.log(id);
+  if (id != "equal" && !otherOperators.includes(id)) {
+    operator = id;
+    if (ArithmeticOperators.includes(id)) {
+      display.innerText += id;
+
+      let first = display.innerText.split(operator)[0];
+      let last = display.innerText.split(operator)[1];
+
+      switch (operator) {
+        case "+":
+          result = +first + +last;
+          break;
+        case "-":
+          result = +first - +last;
+          break;
+        case "*":
+          result = +first * +last;
+          break;
+          78;
+        case "/":
+          result = +first / +last;
+          break;
+      }
+    } else {
+      display.innerText += id;
+    }
+  } else if (!otherOperators.includes(id)) {
+    switch (id) {
+      case "ac":
+        result = "0";
+        break;
+      case "plus-minus":
+        result = `-${display.innerText}`;
+        break;
+      case "*":
+        result = +first * +last;
+        break;
+      case "/":
+        result = +first / +last;
+        break;
+    }
   } else {
-    console.log("ArithmeticOperators:", id);
+    display.innerText = result;
   }
 }
